0ff38894dc3bd4b8fb495cef5a7521848099d5bf
CarlosAlvaZ <87615085+CarlosAlvaZ@users.noreply.github.com>
2021-07-31 15:01:06 -0500

    Commit para hacer push

--- JS/modules.js
+++ JS/modules.js
@@ -1,5 +1,17 @@
-function $(a){
-    return document.querySelector(a);
-}
+import $, {} from './selector.js';
 
-let input = $(".input"), list = $(".input-list");
+
+let button = $('.toggler-button');
+let colapse = $('.colapse');
+let displayed = false;
+button.addEventListener('click', ()=>{
+    if (displayed == false) {
+        colapse.style.display = 'block';
+        displayed = true;
+    }
+    else if (displayed == true) {
+        colapse.style.display = 'none';
+        displayed = false;
+    }
+});
+
--- JS/print.js
+++ JS/print.js
@@ -1,3 +1,17 @@
-function print(){
+// class Item {
+//     'use strict';
+//     constructor(place, toPush){
+//         let place = this.place;
+//         let toPush = this.toPush;
+//     }
+//     getPlace(){
+//         place = document.querySelector(place);
+//     }
+//     posting(){
 
+//     }
+// }
+
+const draw = () => {
+        
 }
